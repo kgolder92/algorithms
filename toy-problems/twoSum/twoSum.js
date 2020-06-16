a3a26e9ff6aff0066d3ae604781f16833f4a2611
Kayla <golderkayla@gmail.com>
2020-06-15 19:49:04 -0700

    complete two sum

---
+++ toy-problems/twoSum/twoSum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const pastNumbers = {};
+  for (let i = 0; i < nums.length; i += 1) {
+    const current = nums[i];
+    const difference = target - current;
+    const matchIndex = pastNumbers[difference];
+    if (matchIndex !== undefined) {
+      return [matchIndex, i];
+    }
+    pastNumbers[current] = i;
+  }
+  return -1;
+};
+
+console.log(twoSum([2, 3, 11, 4, -2], 9));
