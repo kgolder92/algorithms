9ada32ac4c8b14070f070bf992afa995b35c972d
Kayla <golderkayla@gmail.com>
2020-07-25 20:05:18 -0400

    valid palindrome

---
+++ toy-problems/validPalindrome/validPalindrome.js
@@ -0,0 +1,33 @@
+/**
+ * Given a string determine if it is a valid palindrome, considering only alphanumeric characters
+ *
+ * @param {string} s
+ * @return {boolean}
+ *
+ * Example 1:
+    Input: "race a car" => raceacar
+    Output: false
+
+ *  Example 2:
+    Input: "A man, a plan, a canal: Panama" => amanaplanacanalpanama
+    Output: true
+*/
+
+const isPalindrome = (s) => {
+  if (s.length <= 0) return true;
+
+  let filteredStr = '';
+
+  for (let i = 0; i < s.length; i += 1) {
+    const curr = s[i];
+    if (curr.match(/^[A-Za-z0-9]+$/)) {
+      filteredStr += curr;
+    }
+  }
+
+  const result = filteredStr.split('').reverse().join('');
+
+  return filteredStr.toLowerCase() === result.toLowerCase() ? true : false;
+};
+
+console.log(isPalindrome('0p,p0'));
