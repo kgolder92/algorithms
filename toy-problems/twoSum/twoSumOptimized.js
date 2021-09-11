c2f4255b39e726a990ae89181ca85c4b04a681c5
Kayla Golder <golderkayla@gmail.com>
2021-09-11 15:57:42 -0400

    Update twoSumOptimized

--- toy-problems/twoSum/twoSumOptimized.js
+++ toy-problems/twoSum/twoSumOptimized.js
@@ -1,1 +1,20 @@
+O(n log n) time | O(n) space
 
+const twoSum = (array, targetSum) => {
+    let l_ptr = 0;
+    let r_ptr = array.length - 1;
+
+    array.sort((a, b) => a - b);
+
+    while(l_ptr < r_ptr) {
+        const currentSum = array[l_ptr] + array[r_ptr];
+
+        if(currentSum === targetSum) return [array[l_ptr], array[r_ptr]];
+        else if(currentSum < targetSum) {
+            l_ptr++;
+        } else rptr--;
+    }
+    return [];
+}
+
+console.log(twoSum([-4, -1, 1, 3, 5, 6, 8, 11], 10));
